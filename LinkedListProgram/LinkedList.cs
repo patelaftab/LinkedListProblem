cd57b251d43448087bc30427dbc82076a6c618d5
patelaftab <10patelaftab@gmail.com>
2023-02-23 12:08:46 +0530

    [Aftab] :Creating a Simple Linked List

---
+++ LinkedListProgram/LinkedList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProgram
+{
+    public class LinkedList
+    {
+        public Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} Values Are Inserted",node.data);
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if(temp == null) 
+            {
+                Console.WriteLine("Liked List Is Empty");
+            }
+            Console.WriteLine("Linked list IS :");
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data+"");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ LinkedListProgram/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter An Option\n" +
+                    "CHoose 1: For Creating Simple LinkedList\n"+
+                    "Choose 2:To Stop Running");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        linkedList.Add(56);
+                        linkedList.Add(30);
+                        linkedList.Add(70);
+                        linkedList.Display();   
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine();
+                        break;
+
+                }
+            }
+        }
+    }
+}
